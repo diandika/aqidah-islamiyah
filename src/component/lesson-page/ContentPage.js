248cb82fd281819dd5308af190d32d6b86497944
Mohammad Dicky Andika Putra <mohammaddicky.andika@gmail.com>
2021-02-16 14:16:15 +0700

    add content template

--- src/component/lesson-page/ContentPage.js
+++ src/component/lesson-page/ContentPage.js
@@ -1,21 +1,59 @@
 import React, {Component} from "react";
+import "./ContentPage.css";
 
 class ContentPage extends Component{
     render() {
         return(
-            <div className="container">
+            <div className="container content-page">
                 <div className="row">
-                    <div className="col-lg-6">
-                        <div className="content">
-                            konten
-                        </div>
-                        <div className="highlight-ayat">
-                            ayat highlight
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="content-highlight">
+                            <div className="content-box">
+                                <div className="content">
+                                    konten
+                                </div>
+                            </div>
+                            <div className="content-box">
+                                <div className="content">
+                                    ayat highlight
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
                         <div className="ayat-list">
-                            Ayat list
+                            <div className="content">
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                                <div className="ayat-item">
+                                    QS 1:1
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
